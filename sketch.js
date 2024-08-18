525bdda9fd89165fa5408567e38cda8394877258
Mrlightcity <bryantography@gmail.com>
2024-08-18 16:56:27 +0100

    Store a reference of all the nodes to manipulate

---
+++ sketch.js
@@ -0,0 +1,14 @@
+const popUp= document.querySelector('.popup');
+
+const input= document.querySelector('input');
+
+const submit= document.querySelector('input[type="submit"]');
+
+const container= document.querySelector('.container');
+
+const black= document.querySelector('.black button');
+
+const random= document.querySelector('.random button');
+
+const reset= document.querySelector('.reset button');
+console.log(reset)
